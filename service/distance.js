ee0d0c6e9bf2e7256426d43ff14908acf64351e7
M_299785 <m_299785@M-299785s-MBP.hsd1.il.comcast.net>
2020-07-06 17:52:28 -0500

    ENV-36380

--- service/distance.js
+++ service/distance.js
@@ -9,16 +9,14 @@ console.log(zipCodeURL);
 
 var distance = {
    find: function(req, res, next) {
-       request(zipCodeURL + apiKey 
-               + '/distance.json/' + req.params.zipcode1 + '/' 
-               + req.params.zipcode2 + '/mile',
+       request(zipCodeURL + '/mile',
        function (error, response, body) {
            if (!error && response.statusCode == 200) {
                response = JSON.parse(body);
                res.send(response);
            } else {
-               console.log(response.statusCode + response.body);
-               res.send({distance: -1});
+               console.log(response.statusCode + response.body + body);
+               res.send({body: -1});
            }
        });
 
