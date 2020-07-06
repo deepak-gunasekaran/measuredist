ef219f00ff99bce949ea11435fa6c6698aa9d15b
M_299785 <m_299785@M-299785s-MBP.hsd1.il.comcast.net>
2020-07-06 17:40:56 -0500

    ENV-36380

--- service/distance.js
+++ service/distance.js
@@ -1,8 +1,11 @@
 var request = require('request');
-const dhost=process.env.measure_service_host;
-const dport=process.env.measure_service_port;
+const dhost=process.env.NGINX_SERVICE_HOST;
+//const dhost='measureservice';
+const dport=process.env.NGINX_SERVICE_PORT;
+//const dport='measureport';
 const apiKey = process.env.ZIPCODE_API_KEY || "hkCt1nW1wF1rppaEmoor7T9G4ta7R5wFSu8l1dokNz8y53gGZHDneWWVosbEYirC";
-const zipCodeURL = 'https://${dhost}:${dport}/rest/';
+const zipCodeURL = 'http://' + dhost + ':' + dport + '/';
+console.log(zipCodeURL);
 
 var distance = {
    find: function(req, res, next) {
